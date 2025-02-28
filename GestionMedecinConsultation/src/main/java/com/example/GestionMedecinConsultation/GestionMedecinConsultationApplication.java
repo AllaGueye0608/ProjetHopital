a9836001702baef6f24cf2993ea27993edd307f1
alla <a.g51@zig.univ.sn>
2025-02-28 01:02:27 +0000

    ajout des microservices gestions utilisateur

---
+++ GestionMedecinConsultation/src/main/java/com/example/GestionMedecinConsultation/GestionMedecinConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionMedecinConsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMedecinConsultationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMedecinConsultationApplication.class, args);
+	}
+
+}
---
+++ GestionMedecinConsultation/src/test/java/com/example/GestionMedecinConsultation/GestionMedecinConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionMedecinConsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMedecinConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
